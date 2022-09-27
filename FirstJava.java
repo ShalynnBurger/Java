65f5519ad56974fb593ff3f2b76c22ef4f63b035
ShalynnBurger <shalynnburger@gmail.com>
2022-09-27 13:57:29 -0500

    FirstJava

---
+++ FirstJava.java
@@ -0,0 +1,12 @@
+public class FirstJava {
+	public static void main(String[] args) {
+        System.out.println("Sanity Check");
+        String firstName = "Shalynn";
+        String lastName = "Burger";
+        int age = 27;
+        String homeTown = "Chillicothe, IL";
+        System.out.println("My name is" + " "+ firstName + " " + lastName);
+        System.out.println("I am" + age + "years old");
+        System.out.println("My hometown is"+ homeTown);
+	}
+}
